4bfb03b0790fdaf71043aaa1f300123931368120
zhangzhigang <zhangzhigang@tuotiansudai.com>
2017-12-12 17:54:37 +0800

    edit install script

--- etcd-install.sh
+++ etcd-install.sh
@@ -10,26 +10,20 @@ curl -L -o /tmp/etcd.service https://github.com/ttsdzzg/python-repository/raw/ma
 
 tar xzvf /tmp/etcd-v3.2.11-linux-amd64.tar.gz -C /opt/etcd --strip-components=1
 
-/opt/etcd/etcd --version
-
-ETCDCTL_API=3 /opt/etcd/etcdctl version
+alias etcdctl='ETCDCTL_API=3 /opt/etcd/etcdctl'
+alias etcdctl2='ETCDCTL_API=2 /opt/etcd/etcdctl'
 
 mv /tmp/etcd.service /etc/init.d/etcd
 chmod +x /etc/init.d/etcd
 
 chkconfig --add etcd
-
-alias etcdctl='ETCDCTL_API=3 /opt/etcd/etcdctl'
-alias etcdctl2='ETCDCTL_API=2 /opt/etcd/etcdctl'
-
 chkconfig --list etcd
-
 service etcd start
-
-echo "install success"
 
 /opt/etcd/etcd --version
 ETCDCTL_API=3 /opt/etcd/etcdctl version
+
+echo "install success"
 
 
 # rm /tmp/etcd-v3.2.11-linux-amd64.tar.gz
